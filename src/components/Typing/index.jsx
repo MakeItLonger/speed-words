499f103a9247e55406bf74fad410c876c747d4d1
MakeItLonger <zavadiuk@gmail.com>
2021-12-23 13:01:57 +0200

    feature create state for seconds, words and get random quote from arr

--- src/components/Typing/index.jsx
+++ src/components/Typing/index.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 
+const quotes = [
+  'Вы никогда не пересечете океан, если не наберетесь мужества потерять берег из виду',
+  'Как прекрасно, что не нужно ждать ни минуты, чтобы начать делать мир лучше',
+  'Когда я освобождаюсь от того, кто я есть, я становлюсь тем, кем я могу быть',
+  'Каждый хочет изменить человечество, но никто не задумывается о том, как изменить себя',
+  'Не столь важно, как медленно ты идешь, как то, как долго ты идешь, не останавливаясь',
+  'Мудрый человек требует всего только от себя, ничтожный же человек требует всего от других',
+];
+
+const sentence = quotes[Math.floor(Math.random() * quotes.length)];
+const words = sentence.split(' ');
+
 export const Typing = () => {
+  const [sec, setSec] = React.useState(20);
+  const [currentWord, setCurrentWord] = React.useState(words[0]);
+  const [wordsCount, setWordsCount] = React.useState(0);
+
   return (
     <div class="flex typing">
       <p class="typing__enter-word">Введите слово:</p>
-      <h3 class="typing__word">Первое слово</h3>
+      <h3 class="typing__word">{currentWord}</h3>
       <input className="typing__input" type="text" autoFocus={true} />
       <div class="typing__progress">
         <div class="typing__timer">
           <p>Осталось времени:</p>
-          <b>20 сек.</b>
+          <b>{sec} сек.</b>
         </div>
         <div class="typing__counter">
           <p>Введено слов:</p>
-          <b>0</b>
+          <b>{wordsCount}</b>
         </div>
       </div>
     </div>
